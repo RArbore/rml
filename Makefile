697ac15073a3a419921ec53becfea2d9d828fd62
Russel Arbore <russel.jma@gmail.com>
2021-09-05 14:53:47 -0500

    Working test with tensors

--- Makefile
+++ Makefile
@@ -1,8 +1,8 @@
 CC=gcc
 
 install: librml.so
-	cp librml.so /usr/local/lib/librml.so
-	cp rml.h /usr/local/include/rml.h
+	cp librml.so /usr/lib/librml.so
+	cp rml.h /usr/include/rml.h
 librml.so: internal.o tensor.o
 	$(CC) -shared -Wl,-soname,$@ -o $@ $^
 tensor.o: tensor.c rml.h internal.h tensor.h
---
+++ tests/test_tensors.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <rml.h>
+
+int main() {
+    tensor_t *tensor_a = rml_ones_tensor(TENSOR_TYPE_FLOAT, rml_create_dims(2, 2, 3));
+    tensor_t *tensor_b = rml_ones_tensor(TENSOR_TYPE_FLOAT, rml_create_dims(2, 2, 3));
+    rml_tensor_add_inplace(tensor_a, tensor_b);
+    rml_print_tensor(tensor_a);
+}
