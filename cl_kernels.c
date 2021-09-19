d401e999ef2be324893dec3a92ab6a03b6c0cb5f
Russel Arbore <russel.jma@gmail.com>
2021-09-19 14:46:16 -0500

    Fixed compile bugs in cl program

--- cl_kernels.c
+++ cl_kernels.c
@@ -1,24 +1,24 @@
 #include "cl_kernels.h"
 
 const char *rml_cl_program =
-"__kernel void clone(__global TYPE *a, __global TYPE *b)\n"\
+"__kernel void clone(__global float *a, __global float *b)\n"\
 "{\n"\
-"  size_t id = get_global_id(0);\n"\
-"  b[id] = a[id];n"\
+"  unsigned int id = get_global_id(0);\n"\
+"  b[id] = a[id];\n"\
 "}\n"\
-"__kernel void matmul(__global TYPE *a, __global TYPE *b, __global TYPE *c, const size_t d1, const size_t d2, const size_t d3)\n"\
+"__kernel void matmul(__global float *a, __global float *b, __global float *c, const unsigned int d1, const unsigned int d2, const unsigned int d3)\n"\
 "{\n"\
-"  size_t id = get_global_id(0);\n"\
+"  unsigned int id = get_global_id(0);\n"\
 "  c[id] = 0;\n"\
-"  size_t row = id / d3;\n"\
-"  size_t col = id % d3;\n"\
-"  for (size_t i = 0; i < d2; i++) {;\n"\
-"    c[id] += a[row * d1 + iter] * b[iter * d2 + col];\n"\
-"  };\n"\
+"  unsigned int row = id / d3;\n"\
+"  unsigned int col = id % d3;\n"\
+"  for (unsigned int i = 0; i < d2; i++) {;\n"\
+"    c[id] += a[row * d1 + i] * b[i * d2 + col];\n"\
+"  }\n"\
 "}\n"\
-"__kernel void add(__global TYPE *a, __global TYPE *b, __global TYPE *c)\n"\
+"__kernel void add(__global float *a, __global float *b, __global float *c)\n"\
 "{\n"\
-"  size_t id = get_global_id(0);\n"\
+"  unsigned int id = get_global_id(0);\n"\
 "  c[id] = a[id] + b[id];\n"\
 "}\n"\
 "\n";
